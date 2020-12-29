3ad9e7f655293cf70489ad1ffa3f2b2f820383e8
Gent S <gentsemaj@live.com>
2020-12-29 16:29:27 -0500

    Inject every file in the bundle dir instead of just existing ones

--- src/info/ata4/unity/cli/cmd/BundleInjectCmd.java
+++ src/info/ata4/unity/cli/cmd/BundleInjectCmd.java
@@ -9,11 +9,6 @@
  */
 package info.ata4.unity.cli.cmd;
 
-import com.beust.jcommander.Parameters;
-import info.ata4.io.buffer.ByteBufferUtils;
-import info.ata4.io.util.PathUtils;
-import info.ata4.log.LogUtils;
-import info.ata4.unity.asset.bundle.AssetBundle;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
@@ -23,48 +18,60 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.beust.jcommander.Parameters;
+
+import info.ata4.io.buffer.ByteBufferUtils;
+import info.ata4.io.util.PathUtils;
+import info.ata4.log.LogUtils;
+import info.ata4.unity.asset.bundle.AssetBundle;
+
 /**
  *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
-@Parameters(
-    commandNames = "bundle-inject",
-    commandDescription = "Injects extracted files back into asset bundles."
-)
+@Parameters(commandNames = "bundle-inject", commandDescription = "Injects extracted files back into asset bundles.")
 public class BundleInjectCmd extends AssetCommand {
-    
+
     private static final Logger L = LogUtils.getLogger();
-    
+
     public BundleInjectCmd() {
-        setProcessAssets(false);
-        setProcessBundledAssets(false);
+        this.setProcessAssets(false);
+        this.setProcessBundledAssets(false);
     }
-    
+
     @Override
     public void processAssetBundle(AssetBundle bundle) throws IOException {
         Path bundleFile = bundle.getSourceFile();
         Path bundleDir = PathUtils.removeExtension(bundleFile);
-        
+
         // there's no point in injection if the files haven't been extracted yet
         if (Files.notExists(bundleDir)) {
-            L.log(Level.WARNING, "Bundle directory {0} doesn''t exist!", bundleDir);
+            L.log(Level.WARNING, "Bundle directory {0} doesn''t exist!",
+                    bundleDir);
             return;
         }
-        
-        for (Map.Entry<String, ByteBuffer> entry : bundle.getEntries().entrySet()) {
-            String entryName = entry.getKey();
-            Path entryFile = bundleDir.resolve(entryName);
 
-            // replace files in bundle that exist in the directory
+        String[] list = bundleDir.toFile().list();
+        Map<String, ByteBuffer> entries = bundle.getEntries();
+        for (String entryName : list) {
+            Path entryFile = bundleDir.resolve(entryName);
             if (Files.exists(entryFile)) {
-                L.log(Level.INFO, "Injecting {0}", entryName);
-                entry.setValue(ByteBufferUtils.openReadOnly(entryFile));
+                if (entries.containsKey(entryName)) {
+                    L.log(Level.INFO, "Replacing {0}", entryName);
+                    entries.replace(entryName,
+                            ByteBufferUtils.openReadOnly(entryFile));
+                } else {
+                    L.log(Level.INFO, "Inserting {0}", entryName);
+                    entries.put(entryName,
+                            ByteBufferUtils.openReadOnly(entryFile));
+                }
             }
         }
-        
+
         // create backup by renaming the original file
         Path bundleFileBackup = PathUtils.append(bundleFile, ".bak");
-        Files.move(bundleFile, bundleFileBackup, StandardCopyOption.REPLACE_EXISTING);
+        Files.move(bundleFile, bundleFileBackup,
+                StandardCopyOption.REPLACE_EXISTING);
 
         // save bundle to original path
         bundle.save(bundleFile);
