87cf0aa94c1b16223b204ba85e0b81588d480044
Gent S <gentsemaj@live.com>
2020-12-30 16:51:41 -0500

    Add niche bundle-create command

--- src/info/ata4/unity/cli/DisUnityCli.java
+++ src/info/ata4/unity/cli/DisUnityCli.java
@@ -9,10 +9,16 @@
  */
 package info.ata4.unity.cli;
 
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
+
 import info.ata4.log.LogUtils;
 import info.ata4.unity.DisUnity;
+import info.ata4.unity.cli.cmd.BundleCreateCmd;
 import info.ata4.unity.cli.cmd.BundleExtractCmd;
 import info.ata4.unity.cli.cmd.BundleInjectCmd;
 import info.ata4.unity.cli.cmd.BundleListCmd;
@@ -30,94 +36,93 @@ import info.ata4.unity.cli.cmd.LearnCmd;
 import info.ata4.unity.cli.cmd.ListCmd;
 import info.ata4.unity.cli.cmd.SplitCmd;
 import info.ata4.unity.cli.cmd.StatsCmd;
-import java.io.PrintStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * DisUnity command line interface.
- * 
+ *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
 public class DisUnityCli implements Runnable {
-    
+
     private static final Logger L = LogUtils.getLogger();
-    
+
     private final DisUnityOptions opts = new DisUnityOptions();
     private final JCommander jc = new JCommander();
-    
+
     public DisUnityCli() {
-        jc.setProgramName(DisUnity.getProgramName());
-        jc.addObject(opts);
+        this.jc.setProgramName(DisUnity.getProgramName());
+        this.jc.addObject(this.opts);
 
         PrintStream out = System.out;
-        
+
         // asset commands
-        jc.addCommand(new DumpCmd());
-        jc.addCommand(new DumpStructCmd());
-        jc.addCommand(new ExtractCmd());
-        jc.addCommand(new ExtractRawCmd());
-        jc.addCommand(new ExtractTxtCmd());
-        jc.addCommand(new ExtractStructCmd());
-        jc.addCommand(new InfoCmd(out));
-        jc.addCommand(new StatsCmd(out));
-        jc.addCommand(new LearnCmd());
-        jc.addCommand(new ListCmd(out));
-        jc.addCommand(new SplitCmd());
-        
+        this.jc.addCommand(new DumpCmd());
+        this.jc.addCommand(new DumpStructCmd());
+        this.jc.addCommand(new ExtractCmd());
+        this.jc.addCommand(new ExtractRawCmd());
+        this.jc.addCommand(new ExtractTxtCmd());
+        this.jc.addCommand(new ExtractStructCmd());
+        this.jc.addCommand(new InfoCmd(out));
+        this.jc.addCommand(new StatsCmd(out));
+        this.jc.addCommand(new LearnCmd());
+        this.jc.addCommand(new ListCmd(out));
+        this.jc.addCommand(new SplitCmd());
+
         // bundle commands
-        jc.addCommand(new BundleExtractCmd());
-        jc.addCommand(new BundleInjectCmd());
-        jc.addCommand(new BundleListCmd(out));
-        
+        this.jc.addCommand(new BundleExtractCmd());
+        this.jc.addCommand(new BundleInjectCmd());
+        this.jc.addCommand(new BundleListCmd(out));
+        this.jc.addCommand(new BundleCreateCmd());
+
         // debug commands
-        jc.addCommand(new DebugDeserializerCmd());
-        jc.addCommand(new DebugStructDBCmd());
+        this.jc.addCommand(new DebugDeserializerCmd());
+        this.jc.addCommand(new DebugStructDBCmd());
     }
-    
+
     public void parse(String[] args) {
         L.info(DisUnity.getSignature());
-        
-        jc.parse(args);
-        
+
+        this.jc.parse(args);
+
         // display usage
-        if (opts.isHelp()) {
-            jc.usage();
+        if (this.opts.isHelp()) {
+            this.jc.usage();
         }
-        
+
         // increase logging level if requested
-        if (opts.isVerbose()) {
+        if (this.opts.isVerbose()) {
             LogUtils.configure(Level.ALL);
         }
     }
 
     @Override
     public void run() {
-        if (opts.isHelp()) {
+        if (this.opts.isHelp()) {
             return;
         }
-        
-        String cmdName = jc.getParsedCommand();
+
+        String cmdName = this.jc.getParsedCommand();
         if (cmdName == null) {
-            jc.usage();
+            this.jc.usage();
             return;
         }
-        
-        JCommander jcc = jc.getCommands().get(cmdName);
-        
+
+        JCommander jcc = this.jc.getCommands().get(cmdName);
+
         Command cmd = (Command) jcc.getObjects().get(0);
-        cmd.setOptions(opts);
+        cmd.setOptions(this.opts);
         cmd.run();
     }
-    
+
     /**
-     * @param args the command line arguments
+     * @param args
+     *            the command line arguments
      */
     public static void main(String[] args) {
         LogUtils.configure();
-        
+
         DisUnityCli cli = new DisUnityCli();
-        
+
         try {
             cli.parse(args);
             cli.run();
---
+++ src/info/ata4/unity/cli/cmd/BundleCreateCmd.java
@@ -0,0 +1,69 @@
+/*
+ ** 2014 April 20
+ **
+ ** The author disclaims copyright to this source code.  In place of
+ ** a legal notice, here is a blessing:
+ **    May you do good and not evil.
+ **    May you find forgiveness for yourself and forgive others.
+ **    May you share freely, never taking more than you give.
+ */
+package info.ata4.unity.cli.cmd;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.beust.jcommander.Parameters;
+
+import info.ata4.io.buffer.ByteBufferUtils;
+import info.ata4.log.LogUtils;
+import info.ata4.unity.asset.bundle.AssetBundle;
+
+/**
+ *
+ * @author Nico Bergemann <barracuda415 at yahoo.de>
+ */
+@Parameters(commandNames = "bundle-create", commandDescription = "Creates an asset bundle from a template bundle and packs it with the contents of a folder.")
+public class BundleCreateCmd extends AssetCommand {
+
+    private static final Logger L = LogUtils.getLogger();
+
+    public BundleCreateCmd() {
+        this.setProcessAssets(false);
+        this.setProcessBundledAssets(false);
+    }
+
+    @Override
+    public void processAssetBundle(AssetBundle bundle) throws IOException {
+        Path bundleFile = bundle.getSourceFile();
+
+        String[] rootList = bundleFile.getParent().toFile().list();
+        for (String subPath : rootList) {
+            Path subDir = bundleFile.resolveSibling(subPath);
+            if (subDir.toFile().isDirectory()
+                    && !subDir.getFileName().toString().equals("out")) {
+                System.out.println("Packing " + subDir + "...");
+
+                String[] list = subDir.toFile().list();
+                Map<String, ByteBuffer> entries = bundle.getEntries();
+                entries.clear(); // clear the existing entries in memory
+
+                // add new entries
+                for (String entryName : list) {
+                    Path entryFile = subDir.resolve(entryName);
+                    if (Files.exists(entryFile)) {
+                        L.log(Level.INFO, "Inserting {0}", entryName);
+                        entries.put(entryName,
+                                ByteBufferUtils.openReadOnly(entryFile));
+                    }
+                }
+                bundle.save(bundleFile.resolveSibling("out")
+                        .resolve(subDir.getFileName())); // save with same name as directory
+            }
+        }
+    }
+}
